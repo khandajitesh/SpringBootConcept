365f804cc1aec545e47ab1805e91c4771f0cff67
khandajitesh <39655291+khandajitesh@users.noreply.github.com>
2020-08-25 23:31:40 +0530

    1'st commit
    1'st commit

---
+++ src/main/java/com/jk/app/SpringBootConceptApplication.java
@@ -0,0 +1,13 @@
+package com.jk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/app/SpringBootConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
